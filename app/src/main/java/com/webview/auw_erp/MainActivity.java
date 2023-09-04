40aef74b0daff350d8602a4fb420e11df69694c7
@asadul-haque-android <automation26@mis.prangroup.com>
2023-09-04 10:55:37 +0600

    safe commit

--- app/src/main/java/com/webview/auw_erp/MainActivity.java
+++ app/src/main/java/com/webview/auw_erp/MainActivity.java
@@ -1,21 +1,15 @@
-package com.webview.myapplication;
+package com.webview.auw_erp;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.DownloadManager;
-import android.content.Context;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
-import android.webkit.CookieManager;
-import android.webkit.URLUtil;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.Toast;
+
 import androidx.annotation.RequiresApi;
 import androidx.core.app.ActivityCompat;
 
@@ -43,29 +37,29 @@ public class MainActivity extends Activity {
     @Override
     @SuppressLint("SetJavaScriptEnabled")
     protected void onCreate(Bundle savedInstanceState) {
-        requestStoragePermission();
+        //requestStoragePermission();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mWebView = findViewById(R.id.activity_main_webview);
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         mWebView.setWebViewClient(new HelloWebViewClient());
-        mWebView.setDownloadListener((url, userAgent, contentDisposition, mimeType, contentLength) -> {
-            Uri source = Uri.parse(url);
-            DownloadManager.Request request = new DownloadManager.Request(source);
-            String cookies = CookieManager.getInstance().getCookie(url);
-            request.addRequestHeader("cookie", cookies);
-            request.addRequestHeader("User-Agent", userAgent);
-            request.setDescription("Downloading File...");
-            request.setTitle(URLUtil.guessFileName(url, contentDisposition, mimeType));
-            request.allowScanningByMediaScanner();
-            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-            request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, URLUtil.guessFileName(url, contentDisposition, mimeType));
-            DownloadManager dm = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
-            dm.enqueue(request);
-            Toast.makeText(getApplicationContext(), "Downloading File", Toast.LENGTH_LONG).show();
-        });
-        mWebView.loadUrl("https://github.com/satyakami"); //Replace The Link Here
+//        mWebView.setDownloadListener((url, userAgent, contentDisposition, mimeType, contentLength) -> {
+//            Uri source = Uri.parse(url);
+//            DownloadManager.Request request = new DownloadManager.Request(source);
+//            String cookies = CookieManager.getInstance().getCookie(url);
+//            request.addRequestHeader("cookie", cookies);
+//            request.addRequestHeader("User-Agent", userAgent);
+//            request.setDescription("Downloading File...");
+//            request.setTitle(URLUtil.guessFileName(url, contentDisposition, mimeType));
+//            request.allowScanningByMediaScanner();
+//            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+//            request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, URLUtil.guessFileName(url, contentDisposition, mimeType));
+//            DownloadManager dm = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+//            dm.enqueue(request);
+//            Toast.makeText(getApplicationContext(), "Downloading File", Toast.LENGTH_LONG).show();
+//        });
+        mWebView.loadUrl("http://apps.auw.edu.bd:8087/erp/auw/r/auw-erp/"); //Replace The Link Here
     }
     private static class HelloWebViewClient extends WebViewClient
     {
